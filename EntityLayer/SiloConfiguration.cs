7d87585c70cb2c65b59be667c3cf49c0f5e4b1db
Hüseyin YILMAZ <huseyin@yilmaz.me>
2026-03-03 16:33:41 +0300

    Create SiloConfiguration.cs file

---
+++ EntityLayer/SiloConfiguration.cs
@@ -0,0 +1,8 @@
+public class SiloConfiguration
+{
+    public int Id { get; set; }
+    public string SiloCode { get; set; }
+    public int SiloCount { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime UpdatedDate { get; set; }
+}
